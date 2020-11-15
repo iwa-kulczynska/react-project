670b85530a3895b13cd4462aa4eee9656ee59ed8
Iwona Kulczynska <iwa.kulczynska@gmail.com>
2020-11-15 23:15:50 +0100

    fix heroku deploy

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const middlewares = jsonServer.defaults({
-  static: 'dist',
-  noCors: true,
-});
-const port = process.env.PORT || 8080;
+// const jsonServer = require('http-server');
+// const server = jsonServer.create();
+// const middlewares = jsonServer.defaults({
+//   static: 'dist',
+//   noCors: true,
+// });
+// const port = process.env.PORT || 8080;
 
-server.use(middlewares);
-server.listen(port);
+// server.use(middlewares);
+// server.listen(port);
